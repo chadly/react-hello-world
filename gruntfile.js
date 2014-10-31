fc3d961f72a169d6318ffd2617365e4ae4cdbe43
Chad Lee <git@chadly.net>
2014-10-31 11:37:37 -0500

    Inline source maps

--- gruntfile.js
+++ gruntfile.js
@@ -10,7 +10,8 @@ module.exports = function(grunt) {
 				},
 				resolve: {
 					extensions: ['', '.jsx', '.js', '.json']
-				}
+				},
+				devtool: "#inline-source-map"
 			},
 			dist: {
 				entry: "./main",
